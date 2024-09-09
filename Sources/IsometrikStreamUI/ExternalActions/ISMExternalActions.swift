2ae501771a6c02b6a0ce47f011b3dd6a1878ea76
Appscrip 3Embed <>
2024-09-09 15:03:27 +0530

    updated the user access for token model

--- Sources/IsometrikStreamUI/ExternalActions/ISMExternalActions.swift
+++ Sources/IsometrikStreamUI/ExternalActions/ISMExternalActions.swift
@@ -1,6 +1,8 @@
 import UIKit
 import IsometrikStream
 
+public protocol ISMExternalDelegate: ISMGoLiveActionDelegate, ISMStreamActionDelegate {}
+
 final public class ISMExternalActions {
     
     var isometrik: IsometrikSDK
@@ -10,28 +12,51 @@ final public class ISMExternalActions {
     }
     
     @MainActor
-    public func openStream(streamId: String, completion: @escaping(UIViewController?, String?) -> Void) {
+    public func openStream(streamId: String, scene: UIWindowScene?) {
         
-        if streamId.isEmpty {
-            completion(nil,"stream id not found!")
+        if let viewController = getTopViewController(scene: scene), streamId.isEmpty {
+            viewController.view.showToast(message: "stream id not found!")
         }
         
         // get stream data
         let streamQuery = StreamQuery(streamId: streamId)
         
         isometrik.getIsometrik().fetchStreams(streamParam: streamQuery) { streamData in
-            
-            let streams = streamData.streams ?? []
-            let viewModel = StreamViewModel(isometrik: self.isometrik, streamsData: streams)
-            let streamController = StreamViewController(viewModel: viewModel)
-            let navVC = UINavigationController(rootViewController: streamController)
-            navVC.modalPresentationStyle = .fullScreen
-            completion(navVC, nil)
-            
+            self.routeToStream(streamData: streamData, scene: scene)
         } failure: { error in
-            completion(nil, error.localizedDescription)
+            if let viewController = self.getTopViewController(scene: scene) {
+                viewController.view.showToast(message: error.localizedDescription)
+            }
         }
         
+    }
+    
+    func routeToStream(streamData: ISMStreamsData, scene: UIWindowScene?) {
+        
+        let streams = streamData.streams ?? []
+        let viewModel = StreamViewModel(isometrik: self.isometrik, streamsData: streams)
+        let streamController = StreamViewController(viewModel: viewModel)
+        let navVC = UINavigationController(rootViewController: streamController)
+        navVC.modalPresentationStyle = .fullScreen
+        
+        if let topVC = getTopViewController(scene: scene) {
+            topVC.present(navVC, animated: true)
+        }
+        
+    }
+    
+    func getTopViewController(scene: UIWindowScene?) -> UIViewController? {
+        
+        if let windowScene = scene {
+            for window in windowScene.windows {
+                if let rootVC = window.rootViewController {
+                    let topVC = rootVC.topMostViewController()
+                    return topVC
+                }
+            }
+        }
+        
+        return nil
         
     }
     
